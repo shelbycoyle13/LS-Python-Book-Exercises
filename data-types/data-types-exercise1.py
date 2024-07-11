9df2a6ce8dcbde33cf0c7ec5801f5ab54f3123ab
Shelby Coyle <81052674+shelbycoyle13@users.noreply.github.com>
2024-07-11 11:01:48 -0400

    Renamed and adding new exercise

---
+++ data-types/data-types-exercise1.py
@@ -0,0 +1,12 @@
+# Identify the data type or class for each of the following values:
+                #Type
+'True'          #String
+False           #Boolean
+(1, 2, 3)       #Tuple
+1.5             #Float
+[1, 2, 3]       #List
+2               #Integer
+range(5)        #Range
+{1, 2, 3}       #Set
+None            #NoneType
+{'foo': 'bar'}  #Dictionary
