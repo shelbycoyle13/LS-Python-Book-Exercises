0cf8d425836ab58c5525435ffd360e54e7afe734
Shelby Coyle <81052674+shelbycoyle13@users.noreply.github.com>
2024-07-11 11:10:10 -0400

    Adding rest of data types exercises

---
+++ data-types/data-types-exercise2.py
@@ -0,0 +1,11 @@
+# Create a tuple called names that contains several pet names. For instance:
+
+# Name
+# Asta
+# Butterscotch
+# Pudding
+# Neptune
+# Darwin
+# You can use that data or make up your own.
+
+names = (Asta, Butterscotch, Pudding, Neptune, Darwin)
---
+++ data-types/data-types-exercise3.py
@@ -0,0 +1,17 @@
+# Create a dictionary named pets that contains a list of pet names and the type of animal. For instance:
+
+# Name	Animal
+# Asta	dog
+# Butterscotch	cat
+# Pudding	cat
+# Neptune	fish
+# Darwin	lizard
+# You can use that data or make up your own.
+
+pets = {
+    'Asta': 'dog',
+    'Butterscotch': 'cat',
+    'Pudding': 'cat',
+    'Neptune': 'fish',
+    'Darwin': 'lizard',
+}
